a18bf7fefef0a0379d5799847e95d75a14965c23
Rubens Galarraga <rgalarraga121@gmail.com>
2022-07-10 09:45:43 -0300

    actualizamos

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import movie from './movie.json'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { get } from '../utils/httpClient'
+// import movie from './movie.json'
 import styles from './MovieDetails.module.css'
 
 function MovieDetails() {
+  const {movieId} = useParams()
+  console.log(movieId)
+const [movie, setMovie] = useState(null)
+//*********** */
+useEffect(() =>{
+  get("/movie/" + movieId ).then(data =>{
+    setMovie(data)
+  })
+}, [movieId])
+
+if (!movie){
+  return null
+}
+
+//*********** */
     const imagUrl = 'http://image.tmdb.org/t/p/w500' + movie.poster_path;
   return (
     <div className={styles.detailsContainer} >

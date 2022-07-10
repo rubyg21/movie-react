1890691ae2c10cae94b7533cea9c9239e7689e8a
Rubens Galarraga <rgalarraga121@gmail.com>
2022-07-10 12:55:47 -0300

    actualizado

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
 import { get } from '../utils/httpClient';
 
 import MovieCard from './MovieCard'
@@ -7,23 +8,38 @@ import movies from './movies.json'
 import styles from './MoviesGrid.module.css'
 import Spinner from './Spinner';
 
+
+function useQuery(){
+  return new URLSearchParams(useLocation().search)
+}
+
 export function MoviesGrid() {
   //************ */
 
   const [movies, setMovies] =  useState([]);
   const [isLoading, setIsLoading ] = useState(true) 
 
+  const query = useQuery()
+  const search = query.get("search")
+  console.log(search)
+
+
+  const location = useLocation()
+  console.log(location);
 
 
   useEffect(()=>{
     setIsLoading(true)
-  get("/discover/movie").then((data) =>{
+    const searchUrl = search 
+    ? "/search/movie?query=" + search 
+    : "/discover/movie";
+  get(searchUrl).then((data) =>{
        setMovies(data.results)
        setIsLoading(false)
         
     });
 
-  }, []);
+  }, [search]);
    //************ */
 if(isLoading){
   return <Spinner />
